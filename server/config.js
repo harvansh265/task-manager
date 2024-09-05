e33421dc0f3d977d4c65fe15c2ee5d0fdebf53c2
hpwebmart <hpwebmart@gmail.com>
2024-09-05 11:30:41 +0530

    config update

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    mongoURI: '',
-    // mongoURI: 'mongodb://localhost:27017/taskmanager',
+    mongoURI: 'mongodb://localhost:27017/taskmanager',
     jwtSecret: 'secret123',
 };
 
