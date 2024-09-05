4b6e3dc8977f14a9a20d817249b168c7361cc5b0
hpwebmart <hpwebmart@gmail.com>
2024-09-05 11:16:19 +0530

    config update

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://harvansh:Bwmg24UvDL82hD5X@cluster0.qc7v4sj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    mongoURI: '',
     // mongoURI: 'mongodb://localhost:27017/taskmanager',
     jwtSecret: 'secret123',
 };
